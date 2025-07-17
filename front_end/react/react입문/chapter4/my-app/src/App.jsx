5fad809035abb9ea85bb4a223ec602be7d2f8dc8
DESKTOP-N2A6ATC\user <asas87189@gmail.com>
2025-07-17 17:27:44 +0900

    이벤트와 스타일 적용

--- front_end/react/react입문/chapter4/my-app/src/App.jsx
+++ front_end/react/react입문/chapter4/my-app/src/App.jsx
@@ -1,9 +1,18 @@
 export const App = () => {
-	return (
+	//버튼 클릭 시 실행되는 함수 정의
+    const onClickButton = () => {
+        alert();
+    };
+    const contentStyle = {
+        color : "blue",
+        fontSize: "20px"
+    }
+    return(
         <>
-        {console.log("TEST")}
-        <h1>안녕하세요</h1>
-        <p>잘 지내시죠?</p>
+        <h1 style={{color : "red" }}>안녕하세요!</h1>
+        <p style={contentStyle}>잘 지내시죠?</p>
+        <button onClick={onClickButton}>버튼</button>
         </>
-    );
+    )
+
 };
