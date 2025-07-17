29be739ab6aa4274ddce38b475330243fe677a58
DESKTOP-N2A6ATC\user <asas87189@gmail.com>
2025-07-17 15:07:38 +0900

    DOM 요소 추가, 수정

--- front_end/react/react입문/chapter3/src/index.js
+++ front_end/react/react입문/chapter3/src/index.js
@@ -4,3 +4,13 @@ console.log(title1);
 
 const title2 = document.querySelector("#title");
 console.log(title2);
+
+//DOM 작성
+const divEl = document.createElement("div");
+
+const pEl = document.createElement("p");
+const h1El = document.createElement("h1");
+
+divEl.appendChild(pEl);
+divEl.prepend(h1El);
+console.log(divEl);
