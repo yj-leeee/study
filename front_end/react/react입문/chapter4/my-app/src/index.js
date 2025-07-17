33bd6615e44d371911cdea1b002fc7d60d7defca
DESKTOP-N2A6ATC\user <asas87189@gmail.com>
2025-07-17 17:09:30 +0900

    jsx 규칙 1. return이 여려행이면 ()로 감싸기 2. return이후는 한 개의 태그로 둘러싸여야 한다.

--- front_end/react/react입문/chapter4/my-app/src/index.js
+++ front_end/react/react입문/chapter4/my-app/src/index.js
@@ -1,7 +1,12 @@
 import ReactDom from "react-dom";
 
 const App = () => {
-	return null;
+	return (
+        <div>
+        <h1>안녕하세요</h1>
+        <p>잘 지내시죠?</p>
+        </div>
+    );
 };
 
 ReactDom.render(<App />, document.getElementById("root"));
