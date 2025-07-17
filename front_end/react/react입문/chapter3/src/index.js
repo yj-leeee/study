38a5c2acc475e4db5ed4a6186e500862abbf7012
DESKTOP-N2A6ATC\user <asas87189@gmail.com>
2025-07-17 15:11:50 +0900

    버튼 만들어서 영역1입니다 텍스트 밑에 넣기

--- front_end/react/react입문/chapter3/src/index.js
+++ front_end/react/react입문/chapter3/src/index.js
@@ -1,16 +1,8 @@
-console.log("test");
-const title1= document.getElementById("title");
-console.log(title1);
+//영역 1입니다 문자 아래 버튼 생성하기
 
-const title2 = document.querySelector("#title");
-console.log(title2);
+const btnEl = document.createElement("button");
+//버튼 라벨 설정
+btnEl.textContent="버튼";
+const div1 = document.querySelector(".container");
 
-//DOM 작성
-const divEl = document.createElement("div");
-
-const pEl = document.createElement("p");
-const h1El = document.createElement("h1");
-
-divEl.appendChild(pEl);
-divEl.prepend(h1El);
-console.log(divEl);
+div1.appendChild(btnEl);
